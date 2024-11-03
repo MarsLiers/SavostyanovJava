2ef015adc2a43d4cb1532032bc5a83e25e0b3432
SavostyanovB <80152658+MarsLiers@users.noreply.github.com>
2024-11-03 13:51:16 +0400

    Add files via upload

---
+++ Program_2.java
@@ -0,0 +1,22 @@
+package SavostyanovJava;
+
+import java.util.Scanner;
+
+public class Program_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        sc.close();
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        }
+        else {
+            System.out.println("Строки неидентичны");
+        }
+    }
+}
